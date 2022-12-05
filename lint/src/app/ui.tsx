f8a68b5a66e5404a76bf37d3f3ef334c17865b5a
Daryna Parfeniuk <darina.parfeniuk@gmail.com>
2022-12-05 22:00:49 +0200

    button styles

--- lint/src/app/ui.tsx
+++ lint/src/app/ui.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './ui.module.scss';
 
-import Button from './components/Button';
+import Button from './components/BaseButton/BaseButton';
 
 const UI = ({}) => {
   const textbox = React.useRef(undefined);
